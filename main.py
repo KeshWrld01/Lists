124909e3b801770732d61c2127dea8e26bbe824d
KeshWrld01 <124397226+KeshWrld01@users.noreply.github.com>
2026-05-12 11:42:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#lists
+mylist = [1,2,3,5,7,8]
+mylist = ["apple", "mango", "banana", "berry"]
+mylist = ["Giwa", 14, "codingal",68.86]
+print(mylist)
+print(len(mylist))
+print(mylist[0])
+print(mylist[3])
+print(mylist[0:2])
